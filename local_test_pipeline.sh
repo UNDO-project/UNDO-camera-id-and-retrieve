a60ef32ea87a3cc379b928d5bff094d7151665df
jethronap <efthioa@gmail.com>
2026-01-29 16:24:04 +0200

    Updated tests shell (#65)

--- local_test_pipeline.sh
+++ local_test_pipeline.sh
@@ -110,6 +110,11 @@ pytest tests/test_video_stream_integration.py -v
 echo "Done..."
 echo "==============================================="
 
+echo "Running API video upload endpoints tests"
+pytest tests/test_video_upload.py -v
+echo "Done..."
+echo "==============================================="
+
 echo "Running API frame buffer tests"
 pytest tests/test_frame_buffer.py -v
 echo "Done..."
